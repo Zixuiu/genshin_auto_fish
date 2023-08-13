4326bd9be9e26849105a4dc68c9a6569ad494ad1
Zixuiu <105110952+Zixuiu@users.noreply.github.com>
2023-08-13 20:54:47 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,59 +7,55 @@ import os
 import keyboard
 import winsound
 
-parser = argparse.ArgumentParser(description='Train Genshin finsing with DQN')
-parser.add_argument('--batch_size', default=32, type=int)
-parser.add_argument('--n_states', default=3, type=int)
-parser.add_argument('--n_actions', default=2, type=int)
-parser.add_argument('--step_tick', default=12, type=int)
-parser.add_argument('--n_episode', default=400, type=int)
-parser.add_argument('--save_dir', default='./output', type=str)
-parser.add_argument('--resume', default='./weights/fish_sim_net_399.pth', type=str)
+parser = argparse.ArgumentParser(description='Train Genshin fishing with DQN')
+parser.add_argument('--batch_size', default=32, type=int)  # 批大小
+parser.add_argument('--n_states', default=3, type=int)  # 状态数量
+parser.add_argument('--n_actions', default=2, type=int)  # 动作数量
+parser.add_argument('--step_tick', default=12, type=int)  # 步长
+parser.add_argument('--n_episode', default=400, type=int)  # 训练轮数
+parser.add_argument('--save_dir', default='./output', type=str)  # 模型保存路径
+parser.add_argument('--resume', default='./weights/fish_sim_net_399.pth', type=str)  # 恢复训练时的模型路径
 args = parser.parse_args()
 
 if not os.path.exists(args.save_dir):
     os.makedirs(args.save_dir)
 
-net = FishNet(in_ch=args.n_states, out_ch=args.n_actions)
+net = FishNet(in_ch=args.n_states, out_ch=args.n_actions)  # 创建FishNet模型
 if args.resume:
-    net.load_state_dict(torch.load(args.resume))
+    net.load_state_dict(torch.load(args.resume))  # 加载预训练模型
 
-agent = DQN(net, args.batch_size, args.n_states, args.n_actions, memory_capacity=1000)
-#env = Fishing_sim(step_tick=args.step_tick)
+agent = DQN(net, args.batch_size, args.n_states, args.n_actions, memory_capacity=1000)  # 创建DQN agent
+#env = Fishing_sim(step_tick=args.step_tick)  # 创建环境
 env = Fishing(delay=0.1, max_step=150)
 
 if __name__ == '__main__':
-    # Start training
+    # 开始训练
     print("\nCollecting experience...")
     net.train()
     for i_episode in range(args.n_episode):
-        winsound.Beep(500, 500)
-        keyboard.wait('r')
-        # play 400 episodes of cartpole game
-        s = env.reset()
-        ep_r = 0
+        winsound.Beep(500, 500)  # 发出提示音
+        keyboard.wait('r')  # 等待按下'r'键开始训练
+        # 进行400轮的训练
+        s = env.reset()  # 重置环境
+        ep_r = 0  # 记录每轮的总奖励
         while True:
-            if i_episode>200 and i_episode%20==0:
-                env.render()
-            # take action based on the current state
-            a = agent.choose_action(s)
-            # obtain the reward and next state and some other information
-            s_, r, done = env.step(a)
+            if i_episode > 200 and i_episode % 20 == 0:
+                env.render()  # 每隔20轮渲染一次环境
 
-            # store the transitions of states
-            agent.store_transition(s, a, r, s_, int(done))
+            a = agent.choose_action(s)  # 根据当前状态选择动作
+            s_, r, done = env.step(a)  # 执行动作，获取奖励和下一个状态
 
-            ep_r += r
-            # if the experience repaly buffer is filled, DQN begins to learn or update
-            # its parameters.
+            agent.store_transition(s, a, r, s_, int(done))  # 存储状态转移信息
+
+            ep_r += r  # 更新总奖励
             if agent.memory_counter > agent.memory_capacity:
-                agent.train_step()
+                agent.train_step()  # 当经验回放缓冲区满时，开始训练更新模型参数
                 if done:
-                    print('Ep: ', i_episode, ' |', 'Ep_r: ', round(ep_r, 2))
+                    print('Ep: ', i_episode, ' |', 'Ep_r: ', round(ep_r, 2))  # 打印每轮的总奖励
 
             if done:
-                # if game is over, then skip the while loop.
-                break
-            # use next state to update the current state.
-            s = s_
-        torch.save(net.state_dict(), os.path.join(args.save_dir, f'fish_ys_net_{i_episode}.pth'))
+                break  # 如果游戏结束，则跳出循环
+
+            s = s_  # 更新当前状态为下一个状态
+
+        torch.save(net.state_dict(), os.path.join(args.save_dir, f'fish_ys_net_{i_episode}.pth'))  # 保存模型参数
