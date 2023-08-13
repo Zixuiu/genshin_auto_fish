99a536d26e87c61d1f6c6ac6220dac2b072a5e2b
Zixuiu <105110952+Zixuiu@users.noreply.github.com>
2023-08-13 20:58:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-# Copyright (c) Megvii, Inc. and its affiliates. All Rights Reserved
+# -*- coding: utf-8 -*-
+# 上面两行是为了确保脚本在任何环境下都能正确执行
+
+# 版权声明
+# 该脚本是由 Megvii, Inc. 及其关联公司拥有版权，保留所有权利
 
 import re
 import setuptools
@@ -8,10 +12,11 @@ from os import path
 import torch
 from torch.utils.cpp_extension import CppExtension
 
+# 检查 PyTorch 版本是否符合要求
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
 assert torch_ver >= [1, 7], "Requires PyTorch >= 1.7"
 
-
+# 获取扩展模块的信息
 def get_extensions():
     this_dir = path.dirname(path.abspath(__file__))
     extensions_dir = path.join(this_dir, "yolox", "layers", "csrc")
@@ -39,18 +44,18 @@ def get_extensions():
 
     return ext_modules
 
-
+# 从 __init__.py 文件中获取版本号
 with open("yolox/__init__.py", "r") as f:
     version = re.search(
         r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
         f.read(), re.MULTILINE
     ).group(1)
 
-
+# 从 README.md 文件中获取长描述
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
+# 设置包的信息
 setuptools.setup(
     name="yolox",
     version=version,
